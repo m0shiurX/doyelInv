75a364da2aceb68de94589b27f90624e4be9d5b4
Moshiur Rahman <unimrgm@gmail.com>
2023-11-13 06:58:17 +0600

    qty readable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,7 +27,7 @@
                 </div>
 
                 <div class="card-body">
-                    <div class="big_number text-nowrap text-truncate">{{ $stock->quantity ?? '' }}</div>
+                    <div class="big_number text-nowrap text-truncate">{{ number_format($stock->quantity,0) ?? '' }}</div>
                 </div>
             </div>
         </div>
